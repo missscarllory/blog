017dd92c1a1c6fc11facc129c66326ec5998b403
Anne Wang <scarllory576@gmail.com>
2015-03-14 17:02:37 +0800

    Add posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
+  resources :posts
+  root 'posts#index' 
+end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -53,4 +53,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
