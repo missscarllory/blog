5a5789308ec3f0a108ebffe3ca69b5ed376df1af
Anne Wang <scarllory576@gmail.com>
2015-07-11 15:53:39 +0800

    Add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-	def index
+	before_action :authenticate_user!, except: [:index, :show]
+  def index
 		@posts = Post.all.order("created_at DESC")
 	end
 
